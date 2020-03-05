b239bb569d3cb7a3192e61adacc2671e5ee2b4dc
prickly-u <kterehova@gmail.com>
2020-03-05 15:29:02 +0300

    Rollback

--- paperspace/finetune.sh
+++ paperspace/finetune.sh
@@ -1,6 +1,6 @@
 #pip install --upgrade pip
-pip3 install /storage/lacmus/ --user
-python3 /storage/lacmus/setup.py build_ext --inplace
+pip install /storage/lacmus/ --user
+python /storage/lacmus/setup.py build_ext --inplace
 
 
 /storage/lacmus/keras_retinanet/bin/train.py --epoch 1 --steps 10 --config config.ini pascal /storage/lacmus/data/LADDV4_Full
