4254f904633f9f967f0207cc11b7e29bbbbb9d8e
prickly-u <kterehova@gmail.com>
2020-03-09 11:28:20 +0300

    Combined model

--- paperspace/finetune.sh
+++ paperspace/finetune.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-get update
 apt-get install -y libsm6 libxext6 libxrender-dev
 pip install opencv-python
--- paperspace/finetune_part2.sh
+++ paperspace/finetune_part2.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-get update
 apt-get install -y libsm6 libxext6 libxrender-dev
 pip install opencv-python
@@ -19,5 +21,5 @@ mkdir logs/finetuning_part2/gamma_3
 keras_retinanet/bin/train.py --epoch 1 --steps 1000 --tensorboard-freq 10 --config config.ini --no-snapshots --no-random-transform --batch-size 2 --image-min-side 1500 --image-max-side 2000 --focal-gamma 5.0 --tensorboard-dir logs/finetuning_part2/gamma_3 pascal /storage/data/LADDV4_Full >> logs/finetuning_part2/gamma_3/output.log
 
 
-tar -czvf finetuning.tar.gz logs/finetuning_part2
+tar -czvf finetuning2.tar.gz logs/finetuning_part2
 
