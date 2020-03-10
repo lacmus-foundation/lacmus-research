34181ad6aacad53d70d2093e8f9fb4992f5b8e22
prickly-u <kterehova@gmail.com>
2020-03-10 14:30:38 +0300

    Epoch 2

--- paperspace/train_epoch1.sh
+++ paperspace/train_epoch1.sh
@@ -11,7 +11,6 @@ python setup.py build_ext --inplace
 
 mkdir logs/combined_model_e1
 
-mkdir logs/combined_model
 keras_retinanet/bin/train.py --epoch 1 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --lr 0.0001 --image-min-side 1500 --image-max-side 2000 --regression-weight 0.5 --classification-weight 2.0 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e1 pascal /storage/data/LADDV4_Full >> logs/combined_model_e1/output.log
 
 mkdir /artifacts/combined_model
---
+++ paperspace/train_epoch2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y libsm6 libxext6 libxrender-dev
+pip install opencv-python
+
+cd /storage/lacmus/
+
+pip install . --user
+python setup.py build_ext --inplace
+
+mkdir logs/combined_model_e2
+
+keras_retinanet/bin/train.py --snapshot ./snapshots/resnet50_pascal_01.h5 --initial-epoch 1 --epoch 1 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --lr 0.0001 --image-min-side 1500 --image-max-side 2000 --regression-weight 0.5 --classification-weight 2.0 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2/output.log
+
+mkdir /artifacts/combined_model_e2
+cp logs/combined_model_e2/* /artifacts/combined_model_e2/
+cp snapshots/resnet50_pascal_02.h5 /artifacts/combined_model_e2/funetuning_e2.h5  
+
