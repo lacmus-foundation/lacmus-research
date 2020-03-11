0755a5cdad926ae790333524da4c32c2b8168e05
prickly-u <kterehova@gmail.com>
2020-03-11 17:49:36 +0300

    One more attempt. Augmentations & lr=1e-6

--- paperspace/train_epoch2.sh
+++ paperspace/train_epoch2.sh
@@ -7,13 +7,12 @@ pip install opencv-python
 cd /storage/lacmus/
 
 pip install . --user
-python setup.py build_ext --inplace
 
-mkdir logs/combined_model_e2-4
+mkdir logs/combined_model_e2-5
 
-keras_retinanet/bin/train.py --weights ./snapshots/funetuning_e1.h5 --epoch 1 --steps 1000 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --image-min-side 1500 --image-max-side 2000 --lr 0.0001 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2-4 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2-4/output.log
+keras_retinanet/bin/train.py --weights ./snapshots/funetuning_e1.h5 --epoch 1 --tensorboard-freq 100 --config config.ini --batch-size 1 --image-min-side 1500 --image-max-side 2000 --lr 0.000001 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2-5 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2-5/output.log
 
-mkdir /artifacts/combined_model_e2-4
-cp logs/combined_model_e2-4/* /artifacts/combined_model_e2-4/
-cp snapshots/resnet50_pascal_01.h5 /artifacts/combined_model_e2-4/funetuning_e2-4.h5  
+mkdir /artifacts/combined_model_e2-5
+cp logs/combined_model_e2-5/* /artifacts/combined_model_e2-5/
+cp snapshots/resnet50_pascal_01.h5 /artifacts/combined_model_e2-5/funetuning_e2-5.h5  
 
