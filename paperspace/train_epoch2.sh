dce9e00378976dc898bca35cf4010ca22f922acf
prickly-u <kterehova@gmail.com>
2020-03-11 14:46:03 +0300

    short 1000 steps

--- paperspace/train_epoch2.sh
+++ paperspace/train_epoch2.sh
@@ -11,7 +11,7 @@ python setup.py build_ext --inplace
 
 mkdir logs/combined_model_e2-4
 
-keras_retinanet/bin/train.py --weights ./snapshots/funetuning_e1.h5 --epoch 1 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --image-min-side 1500 --image-max-side 2000 --lr 0.0001 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2-4 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2-4/output.log
+keras_retinanet/bin/train.py --weights ./snapshots/funetuning_e1.h5 --epoch 1 --steps 1000 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --image-min-side 1500 --image-max-side 2000 --lr 0.0001 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2-4 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2-4/output.log
 
 mkdir /artifacts/combined_model_e2-4
 cp logs/combined_model_e2-4/* /artifacts/combined_model_e2-4/
