11d92e480e41636fe0f160398034c1c8eec2db6a
prickly-u <kterehova@gmail.com>
2020-03-11 10:52:19 +0300

    Epoch2 without weighted loss

--- paperspace/train_epoch2.sh
+++ paperspace/train_epoch2.sh
@@ -9,11 +9,11 @@ cd /storage/lacmus/
 pip install . --user
 python setup.py build_ext --inplace
 
-mkdir logs/combined_model_e2-2
+mkdir logs/combined_model_e2-3
 
-keras_retinanet/bin/train.py --snapshot ./snapshots/funetuning_e1.h5 --epoch 1 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --image-min-side 1500 --image-max-side 2000 --regression-weight 0.5 --classification-weight 2.0 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2-2 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2-2/output.log
+keras_retinanet/bin/train.py --snapshot ./snapshots/funetuning_e1.h5 --epoch 1 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --image-min-side 1500 --image-max-side 2000 --lr 0.0001 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2-3 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2-3/output.log
 
-mkdir /artifacts/combined_model_e2-2
-cp logs/combined_model_e2-2/* /artifacts/combined_model_e2-2/
-cp snapshots/resnet50_pascal_01.h5 /artifacts/combined_model_e2-2/funetuning_e2.h5  
+mkdir /artifacts/combined_model_e2-3
+cp logs/combined_model_e2-3/* /artifacts/combined_model_e2-3/
+cp snapshots/resnet50_pascal_01.h5 /artifacts/combined_model_e2-3/funetuning_e2-3.h5  
 
