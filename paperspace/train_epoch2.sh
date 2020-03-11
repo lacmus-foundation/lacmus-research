23695582fe931853a26d3224cf3969c95398035f
prickly-u <kterehova@gmail.com>
2020-03-11 14:34:27 +0300

    Load weights, not the full model

--- paperspace/train_epoch2.sh
+++ paperspace/train_epoch2.sh
@@ -9,11 +9,11 @@ cd /storage/lacmus/
 pip install . --user
 python setup.py build_ext --inplace
 
-mkdir logs/combined_model_e2-3
+mkdir logs/combined_model_e2-4
 
-keras_retinanet/bin/train.py --weights ./snapshots/funetuning_e1.h5 --epoch 1 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --image-min-side 1500 --image-max-side 2000 --lr 0.0001 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2-3 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2-3/output.log
+keras_retinanet/bin/train.py --weights ./snapshots/funetuning_e1.h5 --epoch 1 --tensorboard-freq 100 --config config.ini --no-random-transform --batch-size 1 --image-min-side 1500 --image-max-side 2000 --lr 0.0001 --optimizer-clipnorm 0.01 --tensorboard-dir logs/combined_model_e2-4 pascal /storage/data/LADDV4_Full >> logs/combined_model_e2-4/output.log
 
-mkdir /artifacts/combined_model_e2-3
-cp logs/combined_model_e2-3/* /artifacts/combined_model_e2-3/
-cp snapshots/resnet50_pascal_01.h5 /artifacts/combined_model_e2-3/funetuning_e2-3.h5  
+mkdir /artifacts/combined_model_e2-4
+cp logs/combined_model_e2-4/* /artifacts/combined_model_e2-4/
+cp snapshots/resnet50_pascal_01.h5 /artifacts/combined_model_e2-4/funetuning_e2-4.h5  
 
