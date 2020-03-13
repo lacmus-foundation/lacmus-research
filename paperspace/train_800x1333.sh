7ca97495c1b32814acd93a3eb598f75ecabfa38d
prickly-u <kterehova@gmail.com>
2020-03-13 17:31:08 +0300

    Fix folder name

--- paperspace/train_800x1333.sh
+++ paperspace/train_800x1333.sh
@@ -10,7 +10,7 @@ pip install . --user
 
 mkdir logs/retrain_800_1333
 
-keras_retinanet/bin/train.py --weights ./snapshots/funetuning_e4.h5 --epoch 1 --tensorboard-freq 100 --config config.ini --batch-size 1 --optimizer-clipnorm 0.01 --tensorboard-dir logs/logs/retrain_800_1333 pascal /storage/data/LADDV4_Full >> logs/logs/retrain_800_1333/output.log
+keras_retinanet/bin/train.py --weights ./snapshots/funetuning_e4.h5 --epoch 1 --tensorboard-freq 100 --config config.ini --batch-size 1 --optimizer-clipnorm 0.01 --tensorboard-dir logs/retrain_800_1333 pascal /storage/data/LADDV4_Full >> logs/retrain_800_1333/output.log
 
 mkdir /artifacts/logs/retrain_800_1333
 cp logs/logs/retrain_800_1333/* /artifacts/logs/retrain_800_1333/
