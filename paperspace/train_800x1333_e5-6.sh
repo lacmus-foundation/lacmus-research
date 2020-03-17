199e9695bc5fb84263d435ad9a96ecdca50798b8
prickly-u <kterehova@gmail.com>
2020-03-17 10:28:42 +0300

    Epoches 7-8

--- paperspace/train_800x1333_e5-6.sh
+++ paperspace/train_800x1333_e5-6.sh
@@ -12,7 +12,7 @@ mkdir logs/retrain_800_1333_e5-6
 
 keras_retinanet/bin/train.py --weights ./snapshots/finetuning_800_1333_e4.h5 --epoch 2 --tensorboard-freq 100 --config config.ini --batch-size 1 --optimizer-clipnorm 0.01 --tensorboard-dir logs/retrain_800_1333_e5-6 pascal /storage/data/LADDV4_Full >> logs/retrain_800_1333_e5-6/output.log
 
-cp logs/retrain_800_1333_e3-4/* /artifacts
+cp logs/retrain_800_1333_e5-6/* /artifacts
 cp snapshots/resnet50_pascal_01.h5 /artifacts/finetuning_800_1333_e5.h5  
 cp snapshots/resnet50_pascal_02.h5 /artifacts/finetuning_800_1333_e6.h5  
 
