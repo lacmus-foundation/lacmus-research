c3d5533832b1c652d1484891b9092e1ffa23e17f
Ivan Povalyaev <ipovalyaev80@gmail.com>
2021-05-10 09:29:12 +0200

    impement training for lacmus DS on pytorch

---
+++ networks/ModelsMetrics.md
@@ -0,0 +1,43 @@
+|        Model                               |    Full   | Winter   |   Summer |   Spring |
+|--------------------------------------------|-----------|----------|----------|----------|               
+|resnet50_base_best.h5                       |   0.0023  |  0.0054  |  0.0005  |  0.0000  |
+|resnet50_liza_alert_v1.h5                   |   0.6253  |  0.9565  |  0.3242  |  0.2544  |
+|laddv4_summer_epoch2.h5                     |   0.5721  |  0.6705  |  0.7805  |  0.2005  |
+|laddv4_summer_epoch10.h5                    |   0.5744  |  0.5988  |  0.8977  |  0.1632  |
+|finetuning_800_1333_e7.h5                   |   0.9221  |  0.9671  |  0.8915  |  0.8526  |
+|resnet50_2000_1500_inference.h5 (2000x1500) |   0.9536  |  0.9792  |  0.9228  |  0.9147  |
+|mobilenet_v3_small_pascal_07.h5 (1635x981)  |   0.6923  |  0.6302  |  0.8079  |  0.7018  |
+
+
+
+|        Model                               |    Full   | Winter   | Spring   |  SummerM | SummerT  |
+|--------------------------------------------|-----------|----------|----------|----------|----------|               
+|resnet50_liza_alert_v1.h5                   |   0.5605  | 0.9565   | 0.2526   | 0.2734   | 0.5126   |
+|resnet50_liza_alert_prod.h5                 |	 0.7573  | 0.9614   | 0.8408   | 0.8902   | 0.5951   |
+|resnet50_pascal_20_ladd.h5                  |	 0.8443  | 0.9426   | 0.6823   | 0.7080   | 0.8345   |
+|resnet50_pascal_15_1500_2000_ladd.h5        |   0.9356  | 0.9825   | 0.8956   | 0.8839   | 0.9315   |
+
+
+SDD ds metric
+
+|  Model & params                         |    DS     | eval DS | size      | config | mAp    | Epoch |
+| ----------------------------------------|-----------|---------|-----------|--------|--------| ------|
+| keras, from CoCo	                      | peds only | val     | default   |   +    | 0.3391 |   8   |
+| keras, from CoCo                        | all       | val     | defalut   |   +    | 0.3928 |   11  |
+| keras, from CoCo, no-random-transform   | peds only | val     | 1500x2000 |   +    | 0.4197 |   3   |
+| keras, from CoCo, no-random-transform   | all       | val     | 1088x1424 |   +    | 0.3708 |   7   |
+| pretrained SDD                          | peds      | val     | default   |   -    | 0.6030 |   -   |
+| keras, from CoCo                        | peds      | val     | default   |   -    | 0.4286 |   7   |
+| keras, from CoCo                        | peds      | test    | default   |   -    | 0.3200 |   7   |
+| keras, from CoCo,  freeze bb +/         | peds      | val     | default   |   -    | 0.4065 |   19  |
+| keras, from OID, freeze bb +/           | peds      | val     | default   |   -    | 0.4094 |   24  |
+| torch, from CoCo                        | peds      | test    | no_resize |   -    | 0.2238 |   4   |
+| torch, from CoCo                        | peds      | val     | no_resize |   -    | 0.2914 |   8   |
+| torch, from CoCo                        | peds      | val     | 800,1333  |   -    | 0.3601 |   5   | 
+| torch, from CoCo, lr 0.005              | peds      | val     | 800,1333  |   -    | 0.3774 |   5   | 
+
+
+
+
+
+
---
+++ networks/functions_torch.py
@@ -0,0 +1,342 @@
+from collections import defaultdict, deque
+import time
+import datetime
+
+import random
+import math
+
+
+import torch
+import torch.distributed as dist
+import  torchvision.transforms.functional as F
+
+class SmoothedValue(object):
+    """Track a series of values and provide access to smoothed values over a
+    window or the global series average.
+    """
+
+    def __init__(self, window_size=20, fmt=None):
+        if fmt is None:
+            fmt = "{median:.4f} ({global_avg:.4f})"
+        self.deque = deque(maxlen=window_size)
+        self.total = 0.0
+        self.count = 0
+        self.fmt = fmt
+
+    def update(self, value, n=1):
+        self.deque.append(value)
+        self.count += n
+        self.total += value * n
+
+    def synchronize_between_processes(self):
+        """
+        Warning: does not synchronize the deque!
+        """
+        if not is_dist_avail_and_initialized():
+            return
+        t = torch.tensor([self.count, self.total], dtype=torch.float64, device='cuda')
+        dist.barrier()
+        dist.all_reduce(t)
+        t = t.tolist()
+        self.count = int(t[0])
+        self.total = t[1]
+
+    @property
+    def median(self):
+        d = torch.tensor(list(self.deque))
+        return d.median().item()
+
+    @property
+    def avg(self):
+        d = torch.tensor(list(self.deque), dtype=torch.float32)
+        return d.mean().item()
+
+    @property
+    def global_avg(self):
+        return self.total / self.count
+
+    @property
+    def max(self):
+        return max(self.deque)
+
+    @property
+    def value(self):
+        return self.deque[-1]
+
+    def __str__(self):
+        return self.fmt.format(
+            median=self.median,
+            avg=self.avg,
+            global_avg=self.global_avg,
+            max=self.max,
+            value=self.value)
+
+class MetricLogger(object):
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            assert isinstance(v, (float, int))
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError("'{}' object has no attribute '{}'".format(
+            type(self).__name__, attr))
+
+    def __str__(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(
+                "{}: {}".format(name, str(meter))
+            )
+        return self.delimiter.join(loss_str)
+
+    def synchronize_between_processes(self):
+        for meter in self.meters.values():
+            meter.synchronize_between_processes()
+
+    def add_meter(self, name, meter):
+        self.meters[name] = meter
+
+    def log_every(self, iterable, print_freq, header=None):
+        i = 0
+        if not header:
+            header = ''
+        start_time = time.time()
+        end = time.time()
+        iter_time = SmoothedValue(fmt='{avg:.4f}')
+        data_time = SmoothedValue(fmt='{avg:.4f}')
+        space_fmt = ':' + str(len(str(len(iterable)))) + 'd'
+        if torch.cuda.is_available():
+            log_msg = self.delimiter.join([
+                header,
+                '[{0' + space_fmt + '}/{1}]',
+                'eta: {eta}',
+                '{meters}',
+                'time: {time}',
+                'data: {data}',
+                'max mem: {memory:.0f}'
+            ])
+        else:
+            log_msg = self.delimiter.join([
+                header,
+                '[{0' + space_fmt + '}/{1}]',
+                'eta: {eta}',
+                '{meters}',
+                'time: {time}',
+                'data: {data}'
+            ])
+        MB = 1024.0 * 1024.0
+        for obj in iterable:
+            data_time.update(time.time() - end)
+            yield obj
+            iter_time.update(time.time() - end)
+            if i % print_freq == 0 or i == len(iterable) - 1:
+                eta_seconds = iter_time.global_avg * (len(iterable) - i)
+                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+                if torch.cuda.is_available():
+                    print(log_msg.format(
+                        i, len(iterable), eta=eta_string,
+                        meters=str(self),
+                        time=str(iter_time), data=str(data_time),
+                        memory=torch.cuda.max_memory_allocated() / MB))
+                else:
+                    print(log_msg.format(
+                        i, len(iterable), eta=eta_string,
+                        meters=str(self),
+                        time=str(iter_time), data=str(data_time)))
+            i += 1
+            end = time.time()
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print('{} Total time: {} ({:.4f} s / it)'.format(
+            header, total_time_str, total_time / len(iterable)))
+        
+        
+
+def warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor):
+
+    def f(x):
+        if x >= warmup_iters:
+            return 1
+        alpha = float(x) / warmup_iters
+        return warmup_factor * (1 - alpha) + alpha
+
+    return torch.optim.lr_scheduler.LambdaLR(optimizer, f)
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+def reduce_dict(input_dict, average=True):
+    """
+    Args:
+        input_dict (dict): all the values will be reduced
+        average (bool): whether to do average or sum
+    Reduce the values in the dictionary from all processes so that all processes
+    have the averaged results. Returns a dict with the same fields as
+    input_dict, after reduction.
+    """
+    world_size = get_world_size()
+    if world_size < 2:
+        return input_dict
+    with torch.no_grad():
+        names = []
+        values = []
+        # sort the keys so that they are consistent across processes
+        for k in sorted(input_dict.keys()):
+            names.append(k)
+            values.append(input_dict[k])
+        values = torch.stack(values, dim=0)
+        dist.all_reduce(values)
+        if average:
+            values /= world_size
+        reduced_dict = {k: v for k, v in zip(names, values)}
+    return reduced_dict
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
+    model.train()
+    metric_logger = MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+
+    lr_scheduler = None
+    if epoch == 0:
+        warmup_factor = 1. / 1000
+        warmup_iters = min(1000, len(data_loader) - 1)
+
+        lr_scheduler = warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
+
+    for images, targets in metric_logger.log_every(data_loader, print_freq, header):
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = reduce_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        loss_value = losses_reduced.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            print(loss_dict_reduced)
+            sys.exit(1)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+
+        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+
+    return metric_logger
+
+@torch.no_grad()
+def evaluate(model, data_loader, device='cuda'):
+    n_threads = torch.get_num_threads()
+    torch.set_num_threads(1)
+    cpu_device = torch.device("cpu")
+    inference_res = []
+    model.eval()
+    
+    for images, targets in data_loader:
+        images = list(img.to(device) for img in images)
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        outputs = model(images)
+
+        outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+        res = targets, outputs
+        inference_res.append(res)
+
+    torch.set_num_threads(n_threads)
+    return inference_res
+
+
+def collate_fn(batch):
+    """
+    This function helps when we have different number of object instances
+    in the batches in the dataset.
+    """
+    return tuple(zip(*batch))
+
+class ToTensor(object):
+    def __call__(self, image, target):
+        image = F.to_tensor(image)
+        return image, target
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+    
+class RandomHorizontalFlip(object):
+    def __init__(self, prob):
+        self.prob = prob
+
+    def __call__(self, image, target):
+        if random.random() < self.prob:
+            height, width = image.shape[-2:]
+            image = image.flip(-1)
+            bbox = target["boxes"]
+            if bbox.shape[0]>0:
+                bbox[:, [0, 2]] = width - bbox[:, [2, 0]]
+                target["boxes"] = bbox
+        return image, target
+    
+class Resize(object):
+    def __init__(self, target_size):
+        self.target_size = target_size
+    
+    def __call__(self, image, target):
+        old_height, old_width = image.shape[-2:]
+        image = F.resize(image, self.target_size, interpolation=F.InterpolationMode.BILINEAR)
+        new_height, new_width = image.shape[-2:]
+        bbox = target["boxes"]
+        if bbox.shape[0]>0:
+            bbox[:, [0, 2]] = bbox[:, [0, 2]] * new_width / old_width
+            bbox[:, [1, 3]] = bbox[:, [1, 3]] * new_height / old_height
+            target["boxes"] = bbox
+        
+        
+        return image, target 
+    
+def get_transform(train, target_size):
+    transforms = []
+    # converts the image, a PIL image, into a PyTorch Tensor
+    transforms.append(ToTensor())
+    transforms.append(Resize(target_size)) 
+    if train:
+        # during training, randomly flip the training images
+        # and ground-truth for data augmentation
+        transforms.append(RandomHorizontalFlip(0.5))
+    return Compose(transforms)
